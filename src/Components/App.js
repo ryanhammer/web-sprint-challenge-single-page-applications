d299056cd4ce948a95f61d0f08e3b34cada64a28
rhammer35 <rhammer35@icloud.com>
2021-03-26 16:51:38 -0400

    created form in PizzaForm.js and set variables into state to use for form submission and error display in App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,124 @@
 import React from "react";
 import { Route, Link, Switch } from 'react-router-dom';
+import * as yup from 'yup';
+import axios from 'axios';
 
 import Home from './Home'
 import PizzaForm from './PizzaForm'
 import Help from './Help'
 
 const App = () => {
+
+  // Setting initial form values 
+const initialFormValues = {
+  // Text Input for name
+  name: '',
+   // Dropdown Input for pizza size
+  size: '',
+  // Radio Buttons for sauce type
+  sauce: '',
+  // Optional Checkbox for pizza topping choices
+  x_cheese: false,
+  pepperoni: false,
+  sausage: false,
+  ham: false,
+  bacon: false,
+  pineapple: false, 
+  spinach: false,
+  green_peppers: false,
+  // Optional Checkbox for substituting gluten free crust
+  gluten: false,
+  // Text Input for special instructions
+  instructions: ''
+};
+
+// Setting intial values for form validation error messages
+const initialFormErrors = {
+  name: '',
+  size: '',
+  sauce: '',
+};
+
+// Setting initial value for pizza order form submit button as disabled
+const initialDisabled = true;
+
+// Setting variables into State
+
+  /* For Stretch
+  // Setting into state user variable for array of order info which will change on successful form submissions
+  const [order, setOrder] = useState({});
+  */
+  // Setting form values object into state
+  const [formValues, setFormValues] = useState(initialFormValues);
+  // Setting object containing different form field errors into state
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  // Setting boolean variable controlling submit button into state
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  const postNewOrder = newOrder => {
+
+    // Using axios to post successfully submitted form with new order data to backend location
+    axios
+      .post(`https://reqres.in/`, newOrder)
+        .then((res) => {
+          // If post is successful, log order data to the console
+          console.log(res.data);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+        // Form is reset regardless of whether post is successful
+        .finally(setFormValues(initialFormValues))
+  }
+
+  // Event handler functions
+  const inputChange = (name, value) => {
+    // Using yup.reach to use the defined schema to test individual parts of the form for validation
+    // It takes the schema as the first argument, and the key to be tested as the second argument
+    yup
+      .reach(schema, name) 
+      .validate(value)
+      // Clears error if validation is successful
+      .then(() => {
+        setFormErrors({
+          ...formErrors,
+          [name]: '',
+        });
+      })
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          // Validation error from schema if unsuccessful
+          [name]: err.errors[0],
+        });
+      })
+    setFormValues({
+      ...formValues,
+      [name]: value
+    });
+  }
+
+  const formSubmit = () => {
+    const newOrder = {
+      name: formValues.name.trim(),
+      size: formValues.size,
+      sauce: formValues.sauce,
+      toppings: ['x_cheese', 'pepperoni', 'sausage', 'ham', 'bacon', 'pineapple', 'spinach', 'green_peppers'].filter(toppings => formValues[toppings]),
+      gluten: formValues.gluten,
+      instructions: formValues.instructions.trim()
+    };
+    postNewOrder(newOrder);
+  }
+
+  // Employing an effect hook to validate the form each time it is updated allowing for the submit button to be enabled once the form is completely valid
+  useEffect(() => {
+    
+    schema.isValid(formValues).then(valid => {
+      setDisabled(!valid);
+    });
+  }, [formValues])
+
+
   return (
     <>
       {/* Create header to splash across top of any routed location of app */}
@@ -26,7 +139,13 @@ const App = () => {
           <Help />
         </Route>
         <Route path='/pizza' >
-          <PizzaForm />
+          <PizzaForm 
+            values={formValues}
+            change={inputChange}
+            submit={formSubmit}
+            disabled={disabled}
+            errors={formErrors}
+          />
         </Route>
         <Route path='/' component = { Home }/>
       </Switch>
--- src/Components/PizzaForm.js
+++ src/Components/PizzaForm.js
@@ -1,4 +1,244 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom';
 
-export default function PizzaForm() {}
+export default function PizzaForm(props) {
+
+  const { values, submit, change, disabled, errors } = props;
+
+  const onSubmit = evt => {
+    evt.preventDefault();
+    submit();
+  }
+
+  const onChange = evt => {
+    const { name, value, type, checked } = evt.target;
+    const valueToUse = type === 'checkbox' ? checked : value;
+    change(name, valueToUse);
+  }
+
+  return (
+    <main className='form-container'>
+      <h3>Build Your Own Pizza</h3>
+      <img url='../Assets/Pizza.jpg'></img>
+      <h2 id='form-title'>Build Your Own Pizza</h2>
+      <form className='order-form' onSubmit={onSubmit}>
+
+        <section className='form-heading'>
+          <h3>Name for Order</h3>
+          <p>Required</p>
+        </section>
+
+        <section className='form-item'>
+          <input
+              value={values.name}
+              onChange={onChange}
+              name='name'
+              type='text'
+          />
+        </section>
+
+        {/* Dropdown menu for pizza size selection */}
+        <section className='form-heading'>
+          <h3>Choice of Size</h3>
+          <p>Required</p>
+        </section>
+
+        <section className='form-item'>
+          <select
+            onChange={onChange}
+            value={values.size}
+            name='size'
+          >
+
+            <option value=''>- Select an option -</option>
+            <option value='Small'>Small</option>
+            <option value='Medium'>Medium</option>
+            <option value='Large'>Large</option>
+            <option value='X-Large'>X-Large</option>
+
+          </select>
+
+        </section>
+
+        {/* Radio buttons for sauce choice */}
+        <section className='form-heading'>
+          <h3>Choice of Sauce</h3>
+          <p>Required</p>
+        </section>
+
+        <section className='form-item'>
+          <label>Original Red
+            <input
+              type='radio'
+              name='sauce'
+              value='Original Red'
+              onChange={onChange}
+              checked={values.sauce === 'Original Red'}
+            />
+          </label>
+
+          <label>Garlic Ranch
+          <input
+              type='radio'
+              name='sauce'
+              value='Garlic Ranch'
+              onChange={onChange}
+              checked={values.sauce === 'Garlic Ranch'}
+            />
+          </label>
+
+          <label>BBQ Sauce
+            <input
+              type='radio'
+              name='sauce'
+              value='BBQ Sauce'
+              onChange={onChange}
+              checked={values.sauce === 'BBQ Sauce'}
+            />
+          </label>
+
+          <label>Spinach Alfredo
+          <input
+              type='radio'
+              name='sauce'
+              value='Spinach Alfredo'
+              onChange={onChange}
+              checked={values.sauce === 'Spinach Alfredo'}
+            />
+          </label>
+        </section>
+
+        {/* Checkbox section for topping choices */}
+        <section className='form-heading'>
+          <h3>Add Toppings</h3>
+          <p>(Optional) Choose up to 8</p>
+        </section>
+
+        <section className='topping-selection form-item'>
+
+          <section className='topping-list'>
+            <label>Extra Cheese
+              <input
+                type='checkbox'
+                name='x_cheese'
+                onChange={onChange}
+                checked={values.x_cheese}
+              />
+            </label>
+
+            <label>Pepperoni
+              <input
+                type='checkbox'
+                name='pepperoni'
+                onChange={onChange}
+                checked={values.pepperoni}
+              />
+            </label>
+
+            <label>Sausage
+              <input
+                type='checkbox'
+                name='sausage'
+                onChange={onChange}
+                checked={values.sausage}
+              />
+            </label>
+
+            <label>Ham
+              <input
+                type='checkbox'
+                name='ham'
+                onChange={onChange}
+                checked={values.ham}
+              />
+            </label>
+          </section>
+
+          <section className='topping-list'>
+            <label>Bacon
+              <input
+                type='checkbox'
+                name='bacon'
+                onChange={onChange}
+                checked={values.bacon}
+              />
+            </label>
+
+            <label>Pineapple
+              <input
+                type='checkbox'
+                name='pineapple'
+                onChange={onChange}
+                checked={values.pineapple}
+              />
+            </label>
+
+            <label>Spinach
+              <input
+                type='checkbox'
+                name='spinach'
+                onChange={onChange}
+                checked={values.spinach}
+              />
+            </label>
+
+            <label>Green Peppers
+              <input
+                type='checkbox'
+                name='green_peppers'
+                onChange={onChange}
+                checked={values.green_peppers}
+              />
+            </label>
+          </section>
+
+        </section>
+
+        {/* Checkbox section for choice of gluten free crust */}
+        <section className='form-heading'>
+          <h3>Choice of Subsitute</h3>
+          <p>(Optional) Choose up to 1</p>
+        </section>
+
+        <section className='form-item'>
+
+          <label>Gluten Free Crust (+ $1.00)
+            <input
+              type='checkbox'
+              name='gluten'
+              onChange={onChange}
+              checked={values.gluten}
+            />
+          </label>
+
+        </section>
+
+        <section className='form-heading'>
+          <h3>Special Instructions</h3>
+          <p>(Optional)</p>
+        </section>
+
+        <section className='form-item'>
+          <input
+              value={values.instructions}
+              onChange={onChange}
+              name='instructions'
+              type='text'
+          />
+        </section>
+
+        <section className='form-submit'>
+          <section className='errors-section'>
+            <p>{errors.name}</p>
+            <p>{errors.size}</p>
+            <p>{errors.sauce}</p>
+          </section>
+          
+          <button disabled={disabled} id='submitBtn' >Add to Order</button>
+
+        </section>
+
+      </form>
+    </main>
+  )
+}
